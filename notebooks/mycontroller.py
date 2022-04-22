909151ab717761b8c539f856c0aa2f4e792c3d49
garywang918@gmail.com <garywang918@gmail.com>
2022-04-22 12:26:35 +0000

    test

--- notebooks/mycontroller.py
+++ notebooks/mycontroller.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
 print("mycontroller")
+
+# COMMAND ----------
+
+from python.mypython import trans
+
+trans("gary")
---
+++ python/packagewhl.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+!pip install build
+
+# COMMAND ----------
+
+!python -m build . --wheel
